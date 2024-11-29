aac469a6f98899b369a4a04df947a09fdc044d65
ArithaBandara <167397984+ArithaBandara@users.noreply.github.com>
2024-11-29 09:54:18 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java --enable-preview --module-path waiter_jar --add-modules javafx.controls,javafx.fxml -jar waiter_jar/waiter.jar
+jlink/bin/java --enable-preview --module-path waiter_jar --add-modules javafx.controls,javafx.fxml -jar waiter_jar/waiter.jar
